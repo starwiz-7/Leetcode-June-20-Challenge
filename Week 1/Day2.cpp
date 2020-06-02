8321db66b1e22ea50698c5d3a922c556296e70ba
Aryan Yadav <54806954+starwiz-7@users.noreply.github.com>
2020-06-02 14:28:04 +0530

    Create Day2.cpp

---
+++ Week 1/Day2.cpp
@@ -0,0 +1,28 @@
+/*
+  Author: Aryan Yadav(starwiz-7)
+  Delete Node in a Linked List
+
+  Time Complexity:O(1)
+  Difficulty: Easy
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (!node || !node->next) {
+            return;
+        }
+        auto node_to_delete = node->next;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        delete node_to_delete;
+    }
+};
